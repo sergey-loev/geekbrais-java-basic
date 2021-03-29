11830e9fd34cd60c57a866cf6838eaebfdeb2cff
Sergey.Loev <s.loev@docrobot.com>
2021-03-29 17:03:38 +0300

    продолжить дома

--- src/geekbrains/basic/lesson2/HomeWorkApp.java
+++ src/geekbrains/basic/lesson2/HomeWorkApp.java
@@ -1,9 +1,34 @@
 package geekbrains.basic.lesson2;
 
-import geekbrains.basic.lesson1.Main;
-
 public class HomeWorkApp {
     public static void main(String[] args) {
+        int[] arr = {1,0,0,1,1,1,0,1,0,0};
+        inversionArray(arr);
 
     }
+
+    static void inversionArray(int[] arr) {
+
+        String arrInString= "";
+        for(int i=0 ; i<10 ; i++){
+            arrInString = arrInString + "," + arr[i];
+        }
+        System.out.println("arr = ["+ arrInString + "]");
+
+        for(int i=0 ; i<10 ; i++){
+            if (arr[i]==0){
+                arr[i]=1;
+            }else{
+                arr[i]=0;
+            }
+        }
+
+        arrInString= "";
+        for(int i=0 ; i<10 ; i++){
+            arrInString = arrInString + "," + arr[i];
+        }
+        System.out.println("arr = ["+ arrInString + "]");
+
+    }
+
 }
