584629a2d04933f3fdc1ec9f56111a0fd81043af
S.Loev <srrgloi.994@gmail.com>
2021-04-09 11:41:44 +0300

    Продолжить дома

---
+++ src/geekbrains/basic/lesson4/lesson4.java
@@ -0,0 +1,139 @@
+package geekbrains.basic.lesson4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class lesson4 {
+
+    public static int SIZE = 5;
+    public static int DOTS_TO_WIN = 4;
+
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+
+    public static char[][] map;
+
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+    public static boolean checkWin(char symb) {
+        int checkHorizontal;
+        int checkVertical;
+        int checkMainDiagonal = 0;
+        int checkSideDiagonal = 0;
+        boolean isWin = false;
+
+        for(int i = 0; i< map.length; i++) {
+
+            checkHorizontal = 0;
+            checkVertical = 0;
+
+            if (map[i][i] == symb) checkMainDiagonal++;
+            else checkMainDiagonal = 0;
+
+            if (map[i][(map.length - 1) - i] == symb) checkSideDiagonal++;
+            else checkSideDiagonal = 0;
+
+            for (int j = 0; j < map.length; j++) {
+
+                if (map[i][j] == symb) checkHorizontal++;
+                else checkHorizontal = 0;
+
+                if (map[j][i] == symb) checkVertical++;
+                else checkVertical = 0;
+
+                if (checkHorizontal == DOTS_TO_WIN || checkVertical == DOTS_TO_WIN || checkMainDiagonal == DOTS_TO_WIN || checkSideDiagonal == DOTS_TO_WIN) return true;
+
+            }
+        }
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
